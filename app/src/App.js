ced957c3205334e17e21acd6f80bff1eee949330
appsidal_lx <gilielcrack343@gmail.com>
2023-03-30 15:19:48 -0600

    Added dinamic background

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,56 @@
 import "./App.css";
 
 function App() {
-  // crea un nuevo objeto `Date`
   var today = new Date();
+ 
+  var time = today.toLocaleTimeString();
+  var date= today.toLocaleDateString();
+  var now= today.toLocaleString();
 
-  // obtener la fecha y la hora
-  var now = today.toLocaleString();
-  console.log(now);
+  var temporal = new Date();
+
+  temporal.setHours(6,0,0);
+  var amanecerTime= temporal.toLocaleTimeString();
+
+  temporal.setHours(12,0,0);
+  var diaTime= temporal.toLocaleTimeString();
+
+  temporal.setHours(18,0,0);
+  var atardecerTime= temporal.toLocaleTimeString();
+
+  temporal.setHours(21,0,0);
+  var anochecerTime= temporal.toLocaleTimeString();
+
+
+  console.log(time);
+  console.log(anochecerTime);
+  console.log(date);
+  var hora;
+  var tiempo;
+
+  if (time > amanecerTime) {
+    hora="app-sunrise";
+    tiempo="Amanecer";
+  }
+  if (time >diaTime) {
+    hora="app-day";
+    tiempo="Dia";
+  }
+  if (time >atardecerTime) {
+    hora="app-sunset";
+    tiempo="Atardecer";
+  }
+  if(time>anochecerTime){
+    hora="app-night"
+    tiempo="Noche";
+  }
+
   return (
-    <div className="app">
-      <h1>Clima</h1>
+    <div className={hora}>
+      <h3 className="city">TESJo</h3>
+      <p>{now}</p>
+      <p className="temperatura">25/7º</p>
+      <p className="clima">{tiempo}</p>
     </div>
   );
 }
