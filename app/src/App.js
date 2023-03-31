a82faa3c3aa3fc50ba38c5684c4552b2650e87f4
appsidal_lx <gilielcrack343@gmail.com>
2023-03-30 21:32:43 -0600

    Trying to set clima module

--- app/src/App.js
+++ app/src/App.js
@@ -45,12 +45,40 @@ function App() {
     tiempo="Noche";
   }
 
+  var horas= temporal.getHours();
   return (
     <div className={hora}>
       <h3 className="city">TESJo</h3>
       <p>{now}</p>
       <p className="temperatura">25/7º</p>
       <p className="clima">{tiempo}</p>
+      <div className="grid-container">
+        <div class="grid-item">{horas+1}</div>
+        <div class="grid-item">{}</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+        <div class="grid-item">5</div>
+        <div class="grid-item">6</div>
+        <div class="grid-item">7</div>
+        <div class="grid-item">8</div>
+        <div class="grid-item">9</div>
+        <div class="grid-item">10</div>
+        <div class="grid-item">11</div>
+        <div class="grid-item">12</div>
+        <div class="grid-item">13</div>
+        <div class="grid-item">14</div>
+        <div class="grid-item">15</div>
+        <div class="grid-item">16</div>
+        <div class="grid-item">17</div>
+        <div class="grid-item">18</div>
+        <div class="grid-item">19</div>
+        <div class="grid-item">20</div>
+        <div class="grid-item">21</div>
+        <div class="grid-item">22</div>
+        <div class="grid-item">23</div>
+        <div class="grid-item">24</div>
+      </div>
+      
     </div>
   );
 }
--- app/src/index.js
+++ app/src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Clima from './Clima';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function tick() {
+  root.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  );
+}
+
+setInterval(tick, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
