b2c69fb6ac9d7e539f6bcf6ada87d4d274f3b688
appsidal_lx <gilielcrack343@gmail.com>
2023-04-06 14:21:08 -0600

    Mmm, backend and frontend almost done

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -4,9 +4,10 @@ import React, { useState, useEffect } from "react";
 import { faCompass, faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCloud,
-  faCloudBolt,
-  faCloudRain,
+  faCloudMoon,
+  faCloudMoonRain,
+  faCloudSun,
+  faCloudSunRain,
   faWater,
   faWind,
 } from "@fortawesome/free-solid-svg-icons";
@@ -54,7 +55,7 @@ function App() {
   if (time >= anochecerTime) {
     hora = "app-night";
     tiempo = "Noche";
-  }
+  } 
   function fixHour(hour) {
     var system;
 
@@ -81,22 +82,28 @@ function App() {
     style: { animationDuration: "5s", color: "yellow" },
     size: "2xl",
   });
-  const cloud = React.createElement(FontAwesomeIcon, {
-    icon: faCloud,
+  const cloudSun = React.createElement(FontAwesomeIcon, {
+    icon: faCloudSun,
     beat: true,
     style: { animationDuration: "5s", color: "white" },
     size: "2xl",
   });
-  const cloudRain = React.createElement(FontAwesomeIcon, {
-    icon: faCloudRain,
+  const cloudMoon = React.createElement(FontAwesomeIcon, {
+    icon: faCloudMoon,
+    beat: true,
+    style: { animationDuration: "5s", color: "white" },
+    size: "2xl",
+  });
+  const cloudSunRain = React.createElement(FontAwesomeIcon, {
+    icon: faCloudSunRain,
     beat: true,
     style: { animationDuration: "5s", color: "gray" },
     size: "2xl",
   });
-  const cloudBolt = React.createElement(FontAwesomeIcon, {
-    icon: faCloudBolt,
+  const cloudMoonRain = React.createElement(FontAwesomeIcon, {
+    icon: faCloudMoonRain,
     beat: true,
-    style: { animationDuration: "5s", color: "black" },
+    style: { animationDuration: "5s", color: "gray" },
     size: "2xl",
   });
 
@@ -140,15 +147,10 @@ function App() {
   const compass = React.createElement(FontAwesomeIcon, {
     icon: faCompass,
     spin: true,
-    style: { animationDuration: "5s", color: "red" },
+    style: { animationDuration: "5s", color: "gray" },
     size: "5x",
   });
-  var weatherIcon = cloudRain;
-  if (tiempo==="Noche") {
-    weatherIcon=moon;
-  }else{
-     weatherIcon = sunny;
-  }
+  var weatherIcon;
 
   temporal = new Date();
   var horas = temporal.getHours();
@@ -178,6 +180,11 @@ function App() {
     return realDay.toLocaleDateString(realDay.getDate());
   }
   const options = {
+    elements: {
+      point: {
+        radius: 10,
+      },
+    },
     responsive: true,
     plugins: {
       legend: {
@@ -200,6 +207,39 @@ function App() {
   };
   const [ultimaRespuesta, setUltimaRespuesta] = useState(null);
   const [actualTemperatura, setactualTemperatura] = useState(null);
+  const [temperaturas, setTemperaturas] = useState([]);
+  const [lluvias, setlluvias] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost/weatherStation/getNextLluvias.php")
+      .then(function (response) {
+        const nuevasLluvias = [];
+        for (let i = 1; i <= 24; i++) {
+          const lluvia = response.data[i - 1].probabilidad;
+          nuevasLluvias.push(lluvia);
+        }
+        setlluvias(nuevasLluvias);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost/weatherStation/getNextHours.php")
+      .then(function (response) {
+        const nuevasTemperaturas = [];
+        for (let i = 1; i <= 24; i++) {
+          const temperatura = response.data[i - 1].tempeture;
+          nuevasTemperaturas.push(temperatura);
+        }
+        setTemperaturas(nuevasTemperaturas);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   useEffect(() => {
     axios
@@ -216,131 +256,173 @@ function App() {
       });
   }, []);
 
+  function setIcon(probabilidad) {
+    if (probabilidad > 40 && probabilidad < 70 && tiempo !== "Noche") {
+      weatherIcon = cloudSun;
+    } else if (probabilidad > 40 && probabilidad > 70 && tiempo !== "Noche") {
+      weatherIcon = cloudSunRain;
+    } else if (probabilidad > 40 && probabilidad < 70 && tiempo === "Noche") {
+      weatherIcon = cloudMoon;
+    } else if (probabilidad > 40 && probabilidad > 70 && tiempo === "Noche") {
+      weatherIcon = cloudMoonRain;
+    } else if (probabilidad < 40 && tiempo !== "Noche") {
+      weatherIcon = sunny;
+    } else {
+      weatherIcon = moon;
+    }
+
+    return weatherIcon;
+  }
+
   return (
     <div className={hora}>
       <h3 className="city">TESJo</h3>
       <p>{now}</p>
-      <p className="temperatura">{actualTemperatura+"º"}</p>
+      <p className="temperatura">{actualTemperatura + "º"}</p>
       <p className="clima">{tiempo}</p>
       <div className="grid-container">
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[0] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[0])}</p>
+          <p className="climaValue">{lluvias[0] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 1)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[1] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[1])}</p>
+          <p className="climaValue">{lluvias[1] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 2)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[2] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[2])}</p>
+          <p className="climaValue">{lluvias[2] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 3)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[3] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[3])}</p>
+          <p className="climaValue">{lluvias[3] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 4)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[4] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[4])}</p>
+          <p className="climaValue">{lluvias[4] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 5)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[5] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[5])}</p>
+          <p className="climaValue">{lluvias[5] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 6)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[6] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[6])}</p>
+          <p className="climaValue">{lluvias[6] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 7)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[7] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[7])}</p>
+          <p className="climaValue">{lluvias[7] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 8)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[8] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[8])}</p>
+          <p className="climaValue">{lluvias[8] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 9)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[9] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[9])}</p>
+          <p className="climaValue">{lluvias[9] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 10)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[10] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[10])}</p>
+          <p className="climaValue">{lluvias[10] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 11)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[11] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[11])}</p>
+          <p className="climaValue">{lluvias[11] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 12)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[12] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[12])}</p>
+          <p className="climaValue">{lluvias[12] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 13)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[13] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[13])}</p>
+          <p className="climaValue">{lluvias[13] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 14)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[14] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[14])}</p>
+          <p className="climaValue">{lluvias[14] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 15)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[15] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[15])}</p>
+          <p className="climaValue">{lluvias[15] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 16)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[16] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[16])}</p>
+          <p className="climaValue">{lluvias[16] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 17)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[17] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[17])}</p>
+          <p className="climaValue">{lluvias[17] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 18)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[18] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[18])}</p>
+          <p className="climaValue">{lluvias[18] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 19)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[19] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[19])}</p>
+          <p className="climaValue">{lluvias[19] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 20)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[20] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[20])}</p>
+          <p className="climaValue">{lluvias[20] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 21)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[21] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[21])}</p>
+          <p className="climaValue">{lluvias[21] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 22)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[22] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[22])}</p>
+          <p className="climaValue">{lluvias[22] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 23)}</p>
         </div>
         <div>
-          {" "}
-          <p className="climaIcon">{weatherIcon}</p>
+          <p className="climaValue">{temperaturas[23] + "º C"}</p>
+          <p className="climaIcon">{setIcon(lluvias[23])}</p>
+          <p className="climaValue">{lluvias[23] + "%"}</p>
           <p className="climaHour"> {fixHour(horas + 24)}</p>
         </div>
       </div>
@@ -349,43 +431,43 @@ function App() {
           {" "}
           <p>{getDayWeek(0)}</p>
           <p>{getDate(0)}</p>
-          <p>{weatherIcon}</p>
+          <p>{sunny}</p>
         </div>
         <div className="climaIcon">
           {""}
           <p>{getDayWeek(1)}</p>
           <p>{getDate(1)}</p>
-          <p>{weatherIcon}</p>
+          <p>{sunny}</p>
         </div>
         <div className="climaIcon">
           {""}
           <p>{getDayWeek(2)}</p>
           <p>{getDate(2)}</p>
-          <p>{weatherIcon}</p>
+          <p>{sunny}</p>
         </div>
         <div className="climaIcon">
           {""}
           <p>{getDayWeek(3)}</p>
           <p>{getDate(3)}</p>
-          <p>{weatherIcon}</p>
+          <p>{sunny}</p>
         </div>
         <div className="climaIcon">
           {""}
           <p>{getDayWeek(4)}</p>
           <p>{getDate(4)}</p>
-          <p>{weatherIcon}</p>
+          <p>{sunny}</p>
         </div>
         <div className="climaIcon">
           {""}
           <p>{getDayWeek(5)}</p>
           <p>{getDate(5)}</p>
-          <p>{weatherIcon}</p>
+          <p>{sunny}</p>
         </div>
         <div className="climaIcon">
           {""}
           <p>{getDayWeek(6)}</p>
           <p>{getDate(6)}</p>
-          <p>{weatherIcon}</p>
+          <p>{sunny}</p>
         </div>
       </div>
       <div className="clima-chart">
@@ -399,28 +481,28 @@ function App() {
                 label: "Max",
                 data: [21, 24, 27, 20, 25, 30, 25],
                 borderColor: "WHITE",
-                backgroundColor: "red",
+                backgroundColor: "gray",
               },
               {
                 id: 2,
                 label: "Min",
                 data: [7, 5, 1, 2, 5, 9, 5],
                 borderColor: "WHITE",
-                backgroundColor: "blue",
+                backgroundColor: "gray",
               },
             ],
           }}
           options={options}
         />
       </div>
+      <h4>CONFORT</h4>{" "}
       <div className="confort">
-        <h4>CONFORT</h4>{" "}
-        <div className="elementos">
+        <div>
           <p>Humedad</p>
           {water}
           <p>100%</p>
         </div>
-        <div className="elementos">
+        <div>
           {" "}
           <p>Lluvia</p>
           {droplet}
@@ -451,7 +533,9 @@ function App() {
           <p>748 var</p>
         </div>
       </div>
-      <div>La ultima respuesta del servidor fue a las:{" "+ultimaRespuesta}</div>
+      <div>
+        La ultima respuesta del servidor fue a las:{" " + ultimaRespuesta}
+      </div>
     </div>
   );
 }
