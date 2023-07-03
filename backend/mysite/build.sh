7fe1bd205e32f5076d42304cd220b13eab362be3
AppSiDaL <gilielcrack343@gmail.com>
2023-07-02 18:45:22 -0600

    working on py backend

--- backend/mysite/build.sh
+++ backend/mysite/build.sh
@@ -4,5 +4,6 @@ set -o errexit
 pip3 install django
 pip3 install psycopg2
 pip3 install backports.zoneinfo
+pip3 install gunicorn
 python manage.py collectstatic --no-input
 python manage.py migrate
